e4b992f789c30c6d1667b81a13e38888f502896e
rnamboze <154571664+rnamboze@users.noreply.github.com>
2024-11-04 09:47:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,17 @@ my_secret_key = st.secrets['MyOpenAIKey']
 ### Create the LLM API object
 llm = OpenAI(openai_api_key=my_secret_key)
 
-# Define the LangChain Prompt and Chain
+#### Define the Prompt and Chain
 trip_template = """Summarize the following trip experience briefly:{trip_experience}"""
 prompt_template = PromptTemplate(input_variables=["trip_experience"], 
                                  template=trip_template)
 chain = LLMChain(llm=llm, prompt=prompt_template)
 
-# Title of App
+### Title of App
 st.title("Trip Experience Summarizer")
 st.header("Share with us your experience of the latest trip.")
 
-# Textbox for user input
+### Textbox for user input
 trip_experience = st.text_area("Please describe your trip:")
 
 if trip_experience:
