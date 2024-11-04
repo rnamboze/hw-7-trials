62492eeee3f7e44ddfe14f3488a61bf833a35e31
rnamboze <154571664+rnamboze@users.noreply.github.com>
2024-11-04 12:31:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,5 +94,5 @@ trip_experience = st.text_area("Please describe your trip:")
 
 if trip_experience:
     # Run the full chain to get a categorized response
-    result = full_chain.invoke({"trip_experience": trip_experience})
+    result = full_chain.invoke({"request": trip_experience})
     st.write(result)
